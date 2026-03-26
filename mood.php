1e2cad67efcc59cfbef28a1d7f34ee2bb6e5617d
Evans Osumba <osumbaevanzz@gmail.com>
2026-03-26 11:37:23 +0300

    cleaning of project

--- mood.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-// Note: ONNX Runtime PHP is limited; mood prediction is handled client-side in index_after.php
-echo json_encode(['message' => 'Mood prediction handled client-side']);
-?>
